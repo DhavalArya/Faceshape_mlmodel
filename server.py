98a56a5d284151c0af7ddb05dc1f157f9d7bc81d
DhavalArya <55803239+DhavalArya@users.noreply.github.com>
2022-07-08 15:49:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# from urllib.request import Request
 from fastapi import FastAPI, File, Form, UploadFile, Request
 import fastapi
 from fastapi.middleware.cors import CORSMiddleware
@@ -29,7 +28,6 @@ app.add_middleware(
 # define the Input class
 class Input(BaseModel):
     base64str : object
-    # threshold : float
 
 def base64str_to_PILImage(base64str):
     base64_img_bytes = base64str.encode('utf-8')
@@ -43,72 +41,35 @@ def info(name: str):
     return f"hello {name}"
 
 @app.post('/api/predict')
-# async def predict_image(imagepath:Input):
-# bytes = File(...)
 async def predict_image(imagepath: bytes = File(...)):
-    # req_info = await imagepath.json()
     image = Image.open(io.BytesIO(imagepath))
-    # image.show()
-    # print(imagepath)
-    # print(imagepath.base64str)
     images = []
     try:
-        # image = base64str_to_PILImage(imagepath.base64str)
         image = numpy.array(image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, IMAGE_SIZE)
         images.append(image)
     except Exception as e:
         print(f"Broken: {imagepath}")
-    # return "om"
 
     images = numpy.array(images, dtype = 'float32')
     images = images / 255.0
 
     class_names = ['Heart', 'Oblong', 'Oval', 'Round', 'Square']
+    matching_names = {
+        'Oval': 'Varun Dhawan, Hrithik Roshan, Emma Watson, Cameron Diaz',
+        'Oblong': 'Akshay Kumar, Katrina Kaif',
+        'Heart': 'Nick Jonas, Bradley Cooper, Alia Bhatt, Deepika Padukone',
+        'Round': 'Shahid Kapoor, Leonardo Dicaprio, Aishwarya Rai Bacchan',
+        'Square': 'Aamir Khan, Tom Cruise, Jennifer Aniston'
+    }
 
     model = tensorflow.keras.models.load_model('my_model.h5')
-    predictions = model.predict(images)     # Vector of probabilities
-    pred_labels = numpy.argmax(predictions, axis = 1) # We take the highest probability
-    result = class_names[pred_labels[0]]
+    predictions = model.predict(images)    
+    pred_labels = numpy.argmax(predictions, axis = 1) 
+    result = class_names[pred_labels[0]] + '.\\nYour face matches with ' + matching_names[class_names[pred_labels[0]]]
     return result
 
-    # # image = load_image_into_numpy_array(await image.read())
-    # # print(type(image), imagepath)
-    # return {
-    #     'Shape': result
-    # }
-    # face_image = preprocessing.image.load_img(image.file_name, target_size=(150,150))
-
-
-# @app.post('/api/predict')
-# async def predict_image(imagepath:UploadFile = File(...)):
-#     images = []
-#     try:
-#         image = numpy.array(Image.open(imagepath.file))
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#         image = cv2.resize(image, IMAGE_SIZE)
-#         images.append(image)
-#     except Exception as e:
-#         print(f"Broken: {imagepath}")
-    
-#     images = numpy.array(images, dtype = 'float32')
-#     images = images / 255.0
-
-#     class_names = ['Heart', 'Oblong', 'Oval', 'Round', 'Square']
-
-#     model = tensorflow.keras.models.load_model('my_model.h5')
-#     predictions = model.predict(images)     # Vector of probabilities
-#     pred_labels = numpy.argmax(predictions, axis = 1) # We take the highest probability
-#     result = class_names[pred_labels[0]]
-#     print(result)
-
-#     # image = load_image_into_numpy_array(await image.read())
-#     # print(type(image), imagepath)
-#     return {
-#         'Shape': result
-#     }
-#     # face_image = preprocessing.image.load_img(image.file_name, target_size=(150,150))
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host='0.0.0.0')
